b8af24f6e2631892d183b62d7f4207494579278e
fullstakilla <selakovich@mail.ru>
2023-12-05 22:03:30 +0300

    add: primevue

--- src/components/layout/NavbarElement.vue
+++ src/components/layout/NavbarElement.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+  // import test from './navbar/test.vue'
+  import Sidebar from 'primevue/sidebar';
+  import { ref } from 'vue';
 
+  const visible = ref<boolean>(true)
 </script>
 
 <template>
-  <div>
-    navbar
+  <div class="card flex justify-content-center">
+    <Sidebar v-model:visible="visible" header="Sidebar">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Sidebar>
+    <!-- <Button icon="pi pi-arrow-right" @click="visible = true" /> -->
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
---
+++ src/components/layout/navbar/test.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
