dfdf216e0ddea44e98605e921c5d2582bcbb09b3
fullstakilla <selakovich@mail.ru>
2023-12-05 22:47:12 +0300

    add: primeicons

--- src/components/layout/NavbarElement.vue
+++ src/components/layout/NavbarElement.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   // import test from './navbar/test.vue'
   import Sidebar from 'primevue/sidebar';
+  import type Button from 'primevue/button';
   import { ref } from 'vue';
 
   const visible = ref<boolean>(true)
@@ -11,7 +12,7 @@
     <Sidebar v-model:visible="visible" header="Sidebar">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </Sidebar>
-    <!-- <Button icon="pi pi-arrow-right" @click="visible = true" /> -->
+    <Button icon="pi pi-arrow-right" @click="visible = !visible" />
   </div>
 </template>
 
