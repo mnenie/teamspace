6ba5527436ffc1a50c013a7204b4c7df886c8e44
mnenie <121057011+mneniee@users.noreply.github.com>
2023-12-07 23:15:19 +0300

    add: kanban

---
+++ client/src/hooks/useApplyDrag.ts
@@ -0,0 +1,17 @@
+export default function useApplyDarg() {
+  const applyDrag: (arr: any[], dragResult: any) => any[] = (arr, dragResult) => {
+    const { removedIndex, addedIndex, payload } = dragResult;
+    if (removedIndex === null && addedIndex === null) return arr;
+    const result = [...arr];
+    let itemToAdd = payload;
+    if (removedIndex !== null) {
+      itemToAdd = result.splice(removedIndex, 1)[0];
+    }
+    if (addedIndex !== null) {
+      result.splice(addedIndex, 0, itemToAdd);
+    }
+    return result;
+  };
+
+  return { applyDrag };
+}
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HeadPartHome from '@/components/elements/home/HeadPartHome.vue';
+import KanbanBoard from '@/components/elements/home/KanbanBoard.vue';
 </script>
 
 <template>
   <div>
     <HeadPartHome />
+    <KanbanBoard />
   </div>
 </template>
 
