4da792bde05c3de8cedf75a6f8668232f6091eea
mnenie <121057011+mneniee@users.noreply.github.com>
2023-12-08 20:30:32 +0300

    navbar: create folder

--- client/src/components/elements/home/board/Board.vue
+++ client/src/components/elements/home/board/Board.vue
@@ -55,7 +55,7 @@ const bulletColors = (index: number): string => {
 .boards-container {
   display: flex;
   gap: 1.5rem;
-  margin-bottom: 30px;
+  margin-bottom: 20px;
 }
 
 .column-container {
--- client/src/components/elements/home/manager/taskView/Subtask.vue
+++ client/src/components/elements/home/manager/taskView/Subtask.vue
@@ -1,29 +1,117 @@
 <template>
-  <label class="cursor-pointer bg-light-grey dark:bg-very-dark-grey dark:hover:bg-main-purple/25 p-4 flex items-center gap-4 rounded
-  hover:bg-main-purple hover:bg-opacity-25" :for="subtask?.title">
-    <div class="relative">
-      <input class="cursor-pointer " v-model="subtask.isCompleted" type="checkbox" :id="subtask?.title">
-      <div class="h-4 w-4 rounded-sm flex items-center justify-center absolute 
-      top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]"
-        :class="subtask.isCompleted ? checkbox.checked : checkbox.unchecked">
+  <label class="checkbox-label" :class="{'dark-bg': darkMode, 'dark-hover-bg': darkMode}">
+    <div class="checkbox-input" :class="{'cursor-pointer': true, 'checkbox-relative': true, 'checkbox-top-left': true, 'checkbox-translate': true}">
+      <input class="cursor-pointer" v-model="subtask.isCompleted" type="checkbox" :id="subtask?.title">
+      <div class="checkbox-checkbox" :class="subtask.isCompleted ? 'checked' : 'unchecked dark-unchecked'">
         <img v-if="subtask.isCompleted" src="@/assets/icons/icon-check.svg" alt="">
       </div>
     </div>
-    <span class="text-black dark:text-white text-xs font-bold"
-      :class="subtask.isCompleted ? 'line-through text-opacity-50 dark:text-opacity-50' : ''">
+    <span class="checkbox-title" :class="{'line-through': subtask.isCompleted, 'dark-title': darkMode, 'dark-line-through': darkMode}">
       {{ subtask?.title }}
     </span>
   </label>
 </template>
+
 <script setup lang="ts">
+import { defineProps } from 'vue';
 
 const props = defineProps<{
-  subtask: any
-}>()
+  subtask: any,
+  darkMode: boolean,
+}>();
+</script>
 
-const checkbox = {
-  checked: ['bg-main-purple'],
-  unchecked: ['bg-white', 'border', 'border-medium-grey', 'border-opacity-25, dark:bg-dark-grey dark:border-opacity-25']
+<style scoped lang="css">
+.checkbox-label {
+  cursor: pointer;
+  background-color: #f2f2f2;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 0.25rem;
 }
 
-</script>
+.checkbox-label.dark-bg {
+  background-color: #333;
+}
+
+.checkbox-label.dark-hover-bg:hover {
+  background-color: rgba(155, 89, 182, 0.25);
+}
+
+.checkbox-label:hover-bg:hover {
+  background-color: #9b59b6;
+}
+
+.checkbox-label:hover-opacity-bg:hover {
+  background-color: rgba(0, 0, 0, 0.25);
+}
+
+.checkbox-input {
+  position: relative;
+}
+
+.checkbox-input.cursor-pointer {
+  cursor: pointer;
+}
+
+.checkbox-input.checkbox-relative {
+  position: relative;
+}
+
+.checkbox-input.checkbox-top-left {
+  top: 50%;
+  left: 50%;
+}
+
+.checkbox-input.checkbox-translate {
+  transform: translateY(-50%) translateX(-50%);
+}
+
+.checkbox-checkbox {
+  height: 1rem;
+  width: 1rem;
+  border-radius: 0.125rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+}
+
+.checkbox-checkbox.checked {
+  background-color: #9b59b6;
+}
+
+.checkbox-checkbox.unchecked {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-opacity: 0.25;
+}
+
+.checkbox-checkbox.dark-unchecked {
+  background-color: #333;
+  border-opacity: 0.25;
+}
+
+.checkbox-title {
+  font-weight: bold;
+  font-size: 12px;
+  color: #000;
+}
+
+.checkbox-title.line-through {
+  text-decoration: line-through;
+  opacity: 0.5;
+}
+
+.checkbox-title.dark-title {
+  color: #fff;
+  opacity: 0.5;
+}
+
+.checkbox-title.dark-line-through {
+  text-decoration: line-through;
+  opacity: 0.5;
+}
+</style>
