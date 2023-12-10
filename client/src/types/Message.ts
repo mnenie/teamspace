e5f504703910e644d59c15ba312cf46c7299fd72
torderonex <eligorko.info@yandex.ru>
2023-12-10 17:53:48 +0300

    объективно иногда нужно

---
+++ client/src/types/Message.ts
@@ -0,0 +1,10 @@
+export interface IMessage{
+    id : number;
+    userId : number;
+    roomId : number;
+    body : string;
+    
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+}
---
+++ client/src/types/consts.ts
@@ -0,0 +1,5 @@
+export enum TaskStatus {
+    Active = "Активно",
+    Completed = "Завершено",
+    Paused = "Приостановлено",
+}
