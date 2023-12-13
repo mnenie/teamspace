1edd5211e7123ea14e05ac136c821b3dc951e9f6
mnenie <121057011+mneniee@users.noreply.github.com>
2023-12-13 22:24:01 +0300

    add: validation

--- client/src/components/elements/auth/AuthForm.vue
+++ client/src/components/elements/auth/AuthForm.vue
@@ -6,6 +6,34 @@ import { computed, ref } from 'vue';
 import ButtonAuth from '@/components/UI/ButtonAuth.vue';
 import { useUser } from '@/store/user';
 import type { IUser } from '@/types/User';
+import * as yup from 'yup';
+import { useForm } from 'vee-validate';
+
+const {
+  defineInputBinds: defineLoginInputBinds, errors: logErrs, validate: validateLogin } = useForm({
+    validationSchema: yup.object({
+      email: yup.string().email('*Некорректный адрес email').required('*Обязательное поле'),
+      password: yup.string().min(8, '*Пароль должен содержать не менее 8 символов').required('*Обязательное поле'),
+    }),
+  });
+const email = defineLoginInputBinds('email');
+const password = defineLoginInputBinds('password');
+
+const {
+  defineInputBinds: defineRegistrationInputBinds, errors: regErrs, validate: validateRegistration, } = useForm({
+    validationSchema: yup.object({
+      emailRegVal: yup.string().email('*Некорректный адрес email').required('*Обязательное поле'),
+      passwordRegVal: yup.string().min(8, '*Пароль должен сожержать не менее 8 символов').required('*Обязательное поле'),
+      nameVal: yup.string().required('*Обязательное поле'),
+      lastNameVal: yup.string().required('*Обязательное поле'),
+    }),
+  });
+const emailRegVal = defineRegistrationInputBinds('emailRegVal');
+const passwordRegVal = defineRegistrationInputBinds('passwordRegVal');
+const nameVal = defineRegistrationInputBinds('nameVal');
+const lastNameVal = defineRegistrationInputBinds('lastNameVal');
+
+
 const route = useRoute();
 
 const text = computed(() => {
@@ -23,30 +51,35 @@ const user = useUser()
 const router = useRouter()
 
 const onRegistration = async () => {
-  const registr : IUser = {
+  await validateRegistration();
+  const registr: IUser = {
     email: emailReg.value,
     password: passwordReg.value,
     username: name.value + ' ' + lastname.value
   }
-  await user.userRegistration(registr)
-  router.push(HOME_ROUTE)
-  emailReg.value = ''
-  passwordReg.value = ''
-  name.value = ''
-  lastname.value = ''
+  if (Object.keys(regErrs.value).length === 0) {
+    await user.userRegistration(registr)
+    router.push(HOME_ROUTE)
+    emailReg.value = ''
+    passwordReg.value = ''
+    name.value = ''
+    lastname.value = ''
+  }
 }
 
 const onAuth = async () => {
+  await validateLogin();
   const auth = {
     email: emailAuth.value,
     password: passwordAuth.value,
   }
-  await user.userLogin(auth)
-  router.push(HOME_ROUTE)
-  emailAuth.value = ''
-  passwordAuth.value = ''
+  if (Object.keys(logErrs.value).length === 0) {
+    await user.userLogin(auth)
+    router.push(HOME_ROUTE)
+    emailAuth.value = ''
+    passwordAuth.value = ''
+  }
 }
-
 
 </script>
 
@@ -54,17 +87,31 @@ const onAuth = async () => {
   <div class="form_auth">
     <h2 class="title">{{ text }}</h2>
     <div v-if="$route.path === AUTH_ROUTE" class="form_inputs_auth">
-      <InputFormAuth v-model="emailAuth" :plholder="'Введите email'" :type="'text'" />
-      <InputFormAuth v-model="passwordAuth" :plholder="'Введите пароль'" :type="'password'" />
+      <InputFormAuth v-bind="email" v-model="emailAuth" :plholder="'Введите email'" :type="'text'" />
+      <p v-if="logErrs.email" style="margin-top: -5px; margin-bottom: 10px;" class="modal__error">{{ logErrs.email }}</p>
+      <InputFormAuth v-bind="password" v-model="passwordAuth" :plholder="'Введите пароль'" :type="'password'" />
+      <p v-if="logErrs.password" style="margin-top: -5px; margin-bottom: 10px;" class="modal__error">{{ logErrs.password
+      }}
+      </p>
     </div>
     <div v-if="$route.path === REGISTRATION_ROUTE" class="form_inputs_reg">
-      <InputFormAuth v-model="emailReg" :plholder="'Введите email'" :type="'text'"/>
-      <InputFormAuth v-model="name" :plholder="'Введите имя'" :type="'text'"/>
-      <InputFormAuth v-model="lastname" :plholder="'Введите фамилию'" :type="'text'"/>
-      <InputFormAuth v-model="passwordReg" :plholder="'Введите пароль'" :type="'password'"/>
+      <InputFormAuth v-model="emailReg" v-bind="emailRegVal" :plholder="'Введите email'" :type="'text'" />
+      <p v-if="regErrs.emailRegVal" style="margin-top: -5px; margin-bottom: 10px;" class="modal__error">{{
+        regErrs.emailRegVal }}</p>
+      <InputFormAuth v-model="name" v-bind="nameVal" :plholder="'Введите имя'" :type="'text'" />
+      <p v-if="regErrs.nameVal" style="margin-top: -5px; margin-bottom: 10px;" class="modal__error">{{
+        regErrs.nameVal }}</p>
+      <InputFormAuth v-model="lastname" v-bind="lastNameVal" :plholder="'Введите фамилию'" :type="'text'" />
+      <p v-if="regErrs.lastNameVal" style="margin-top: -5px; margin-bottom: 10px;" class="modal__error">{{
+        regErrs.lastNameVal }}
+      </p>
+      <InputFormAuth v-model="passwordReg" v-bind="passwordRegVal" :plholder="'Введите пароль'" :type="'password'" />
+      <p v-if="regErrs.passwordRegVal" style="margin-top: -5px; margin-bottom: 10px;" class="modal__error">{{
+        regErrs.passwordRegVal }}</p>
     </div>
     <ButtonAuth v-if="route.path === AUTH_ROUTE" @click="onAuth" style="margin-top: 20px;">Войти</ButtonAuth>
-    <ButtonAuth v-if="route.path === REGISTRATION_ROUTE" @click="onRegistration" style="margin-top: 20px;">Зарегистрироваться</ButtonAuth>
+    <ButtonAuth v-if="route.path === REGISTRATION_ROUTE" @click="onRegistration" style="margin-top: 20px;">
+      Зарегистрироваться</ButtonAuth>
   </div>
 </template>
 
--- client/src/components/elements/home/FiltersElement.vue
+++ client/src/components/elements/home/FiltersElement.vue
@@ -1,37 +1,35 @@
 <script setup lang="ts">
 import { ref ,watch} from 'vue';
 import type { IFilters } from '@/types/filters.interface';
-import { HOME_ROUTE, EDITOR_ROUTE } from '../../../utils/consts';
+import { HOME_ROUTE, EDITOR_ROUTE, SETTINGS_ROUTE } from '../../../utils/consts';
 import { useRouter } from 'vue-router';
 import { BOARD_ROUTE } from '../../../utils/consts';
 import { useBoard } from '@/store/board';
 import { useProject } from '@/store/project';
 const filters = ref<IFilters[]>([
   { id: 0, active: true, title: 'Доска' },
-  { id: 1, active: false, title: 'Идея' },
-  { id: 2, active: false, title: 'Настройки' },
+  { id: 1, active: false, title: 'Участники' },
+  { id: 2, active: false, title: 'Магазин' },
 ])
 
 const board = useBoard();
 const project = useProject();
 const router = useRouter()
 
-watch(() => project.project, () => {
-  filterChanges((2));
-})
+
 
 const filterChanges = (id: number) => {
   filters.value.forEach((btn, i) => {
     btn.active = i === id;
   });
   if (id === 0) {
-    router.push(board.boards[0] ? BOARD_ROUTE + '/' + board.boards[0].id :HOME_ROUTE )
+    router.push(board.boards[0] ? BOARD_ROUTE + '/' + board.boards[0].id : HOME_ROUTE )
   } 
   if (id === 1) {
-    router.push(EDITOR_ROUTE)
+    router.push(HOME_ROUTE)
   }
   if (id === 2) {
-    // router.push(HOME_ROUTE )
+    router.push(HOME_ROUTE)
   }
 }
 
