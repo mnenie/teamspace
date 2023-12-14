59021bea26815435a15b2cecfc461af19567510a
torderonex <eligorko.info@yandex.ru>
2023-12-14 14:51:14 +0300

    fix

--- client/src/services/BoardService.ts
+++ client/src/services/BoardService.ts
@@ -89,7 +89,7 @@ export default class BoardService {
      * @returns A Promise resolving to the AxiosResponse indicating the success of the operation.
      */
     static async completeTask(id: string | number): Promise<AxiosResponse<void>> {
-        const resp = await $api.post("/board/task/complete" + id);
+        const resp = await $api.post("/board/task/complete/" + id);
         return resp;
     }
 
