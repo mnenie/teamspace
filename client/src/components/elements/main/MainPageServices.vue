ed674f0434d15bb55062e265ef5f0ee63173e8b2
mnenie <121057011+mneniee@users.noreply.github.com>
2023-12-14 21:31:18 +0300

    fix: mainpage

--- client/src/components/elements/main/MainPageServices.vue
+++ client/src/components/elements/main/MainPageServices.vue
@@ -16,6 +16,7 @@ const pages = ref<Array<page>>([
     {title: 'Записывай', description: 'В каждом проекте пользователь может создавать свои документации и работать с ними. Количество неограничено.', src: '/img/sheet.png', inverted: true, type: 'sheets'},
     {title: 'Общайся', description: 'В каждом проекте пользователь может создавать свои чаты и общаться с другими. Количество неограничено.', src: '/img/chat.jpg', inverted: false, type: 'chats'},
     {title: 'Планируй', description: 'В каждом проекте пользователь может создавать свои дорожные карты и выставлять цели. Количество неограничено.', src: '/img/roadmap.jpg', inverted: true, type: 'roadmaps'},
+    {title: 'Покупай', description: 'В каждом проекте пользователь при выполнении задач получает баллы и в дальнейшем может обменять на товары от компании', src: '/img/store.png', inverted: true, type: 'roadmaps'},
 ])
 
 watch(breakInvert, (newValue) => {
